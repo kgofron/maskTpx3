b8c065d658a8ea69167d1236b3af809948004512
K. Gofron <kgofron@comcast.net>
2025-04-02 18:54:14 -0400

    Update documentation of x,y chip computations

--- xyChip/xyChip.sh
+++ xyChip/xyChip.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Computes x,y chip coordinates of masked pixels in chip coordinates
 gcc -o check_bit check_bit.c
 ./check_bit  Eq_neg_cfg1.bpc
+
+# OUTPUT
+#
+#Position	Value	Chip	X	Y
+#--------	-----	----	-	-
+#18291		0x01	0	115	71
+#18292		0x01	0	116	71
+#24812		0x01	0	236	96
 
