eead33e5c6539064b7140d303b48c333e6f36b55
K. Gofron <kgofron@comcast.net>
2025-08-14 09:49:05 -0400

    Update documentation in xyChip.sh

--- xyChip/xyChip.sh
+++ xyChip/xyChip.sh
@@ -5,10 +5,13 @@ gcc -o check_bit check_bit.c
 ./check_bit  Eq_neg_cfg1.bpc
 
 # OUTPUT
-#
-#Position	Value	Chip	X	Y
-#--------	-----	----	-	-
-#18291		0x01	0	115	71
-#18292		0x01	0	116	71
-#24812		0x01	0	236	96
 
+# File: ~/Documents/Detector/TimePix3/41000039Venus/usb/high-power/eq-accos-02.bpc
+# Orientation=LEFT
+# ------------------------------------------------------------------------
+# #	Position	Value	Chip	x	y	X	Y
+# -	--------	-----	----	-	-	-	-
+# 1	964		0x1F	0	196	3	508	59
+# 2	3569		0x1F	0	241	13	498	14
+# 3	3570		0x1F	0	242	13	498	13
+# 4	3968		0x01	0	128	15	496	127
